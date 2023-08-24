320379ef88c1b4e4ca853345d4f9b505fb863f97
HyoSung Bidol-Lee <hyosung11@me.com>
2023-08-24 17:24:48 -0400

    Create the `/search` route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const express = require("express");
 const path = require("path");
 
@@ -11,6 +12,25 @@ app.get("/", (req, res) => {
   res.render("home", {
     title: "Search Hacker News",
   });
+});
+
+async function searchHN(query) {
+  const response = await axios.get(
+    `https://hn.algolia.com/api/v1/search?query=${query}&tags=story&hitsPerPage=90`
+  );
+
+  return response.data;
+}
+
+app.get('/search', async (req, res) => {
+  const searchQuery = req.query.q;
+  if (!searchQuery) {
+    res.redirect(302, '/');
+    return;
+  }
+
+  const results = await searchHN(searchQuery);
+  res.status(200).json(results);
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
